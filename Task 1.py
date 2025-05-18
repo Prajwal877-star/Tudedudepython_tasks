bcf7f4a8950c111dd14591acfc3633cb3e467a73
Prajwal877-star <prajwaldadas877@gmail.com>
2025-05-18 14:49:10 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,17 @@
+#Task 1
+
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+a = int(a)
+b = int(b)
+add = a + b
+sub = a - b
+multiply = a * b
+divide = a/b
+divide = float(divide)
+
+print('Addition: ',add)
+print('Subtraction: ',sub)
+print('Multiplication: ',multiply)
+print('Division: ',divide)
+
---
+++ Task 2.py
@@ -0,0 +1,7 @@
+#Task 2
+first_name = input('Enter the first name:')
+last_name = input('Enter the last name:')
+
+complete_name = (first_name + " " + last_name)
+
+print('Hello, ' + complete_name + '! Welcome to the Python program.')
